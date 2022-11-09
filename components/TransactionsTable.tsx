237db81a5488b9574d41419e2e8ba8cc6e69ab4f
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-11-09 13:24:41 +0530

    chore(components): wip save state for now

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import Link from 'next/link'
 import { transactions } from '../utils/transactions'
 import { data } from '../utils/data';
-import { transition } from 'd3';
+import { XCircleIcon } from '@heroicons/react/solid';
 
-
+// TODO: See https://stackoverflow.com/questions/53574614/multiple-calls-to-state-updater-from-usestate-in-component-causes-multiple-re-re
+// TODO: USE id to keep readonly elements all individual...
+//
 export default function BucketsTable() {
     const [disabled, setDisabled] = useState(false)
     const handleClickEditTransaction = () => { setDisabled(!disabled) }
@@ -36,9 +37,7 @@ export default function BucketsTable() {
                     <tr>
                         {/* Radio check */}
                         <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
+                            <label><input type="checkbox" className="checkbox" /></label>
                         </th>
                         {/* Posted */}
                         <th>
@@ -57,14 +56,7 @@ export default function BucketsTable() {
                                         {data.accounts.map((account) => <option>{account.type}</option>)}
                                     </select>
                                     <span className="absolute pointer-events-none bg-transparent text-info inset-y-0 right-0 ml-3 flex items-center pr-2">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth={2}
-                                            stroke="currentColor"
-                                            className="w-6 h-6"
-                                        >
+                                        <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" >
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                                         </svg>
                                     </span>
@@ -216,7 +208,6 @@ export default function BucketsTable() {
                                         </span>
                                     </div>
                                 </th>
-
                                 {/* Actions */}
                                 {/* TODO: Use useEffect to maintain active editable state for all buttons */}
                                 <th>
@@ -224,8 +215,10 @@ export default function BucketsTable() {
                                         {/* no-symbol */}
                                         <button
                                             onClick={handleClickClearTransaction}
-                                            className={`${disabled
-                                                ? 'opacity-100' : 'opacity-0'
+                                            key={transaction.id}
+                                            disabled={disabled}
+                                            className={`${!disabled
+                                                ? 'opacity-100' : 'opacity-80'
                                                 } btn btn-square btn-ghost`
                                             }
                                         >
@@ -233,36 +226,39 @@ export default function BucketsTable() {
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
                                             </svg>
                                         </button>
-
                                         {/* // done check mark. */}
-                                        {disabled ? (
-                                            <button
-                                                onClick={handleClickEditTransaction}
-                                                className='btn btn-square btn-ghost'
-                                                key={1}
-
-                                            // className={`${disabled ? 'block' : 'hidden'} btn btn-square btn-ghost`}>
-                                            // className='btn btn-square btn-ghost'
-                                            >
-                                                {/* className={`${disabled ? 'block' : 'hidden'} btn btn-square btn-ghost`}> */}
-                                                <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                                                </svg>
-                                            </button>
-                                        ) :
-                                            <button
-                                                onClick={handleClickEditTransaction}
-                                                className='btn btn-square btn-ghost'
-                                                disabled={disabled}
-                                            >
-                                                <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                                </svg>
-                                            </button>
-                                        }
+                                        <button
+                                            onClick={handleClickEditTransaction}
+                                            // and id is not the one clicked?
+                                            className={`btn btn-square btn-ghost ${!disabled ? 'block' : 'hidden'}`}
+                                            key={transaction.id}
+                                            disabled={disabled}
+                                        >
+                                            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                            </svg>
+                                        </button>
+                                        {/* // pencil edit. */}
+                                        <button
+                                            onClick={handleClickEditTransaction}
+                                            className={`btn btn-square btn-ghost ${disabled ? 'block' : 'hidden'}`}
+                                            key={transaction.id}
+                                            disabled={!disabled}
+                                        >
+                                            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>
+                                        </button>
                                     </div>
                                 </th>
-
+                                { /* Fragments <> allow <select> to have multiple childen. */}
+                                {/* <option key={x} value={x} > {x} </option> */}
+                                {/* // Using --downlevelIteration flag to iterate. */}
+                                {/* {transactions.map((transact: any) => ( */}
+                                {/* <select value={transaction.amount}> */}
+                                {/*     <> {[...Array(transaction.amount).keys()].map((x) => (<option>{x + 1}</option>))} </> */}
+                                {/* </select> */}
+                                {/* ))} */}
                             </tr>
                         ))
                     }
@@ -271,7 +267,7 @@ export default function BucketsTable() {
                 {/* <!-- foot --> */}
                 <tfoot>
                     <tr>
-                        <th> </th>
+                        <th></th>
                         <th>Posted</th>
                         <th>Account</th>
                         <th>Memo</th>
@@ -316,159 +312,58 @@ const statistics = () => (
     </svg>
 )
 
-export function NewTransaction() {
-    return (
-        <tr>
-            <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
-            </th>
+// https://stackoverflow.com/a/61176559
+/* const useData = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    data: undefined,
+    status: "loading"
+  });
 
-            {/* Posted */}
-            <th>
-                <input
-                    type="text"
-                    placeholder="31/12/22"
-                    className="input input-md w-24 sm:w-32  input-bordered "
-                />
-            </th>
+  useEffect(() => {
+    fetchData_fakeApi().then(test => {
+      if (test.ok) dispatch(["success", test.data.results]);
+    });
+  }, []);
 
-            {/* Account */}
-            <th>
-                <div className="form-control">
-                    <div className="input-group relative">
-                        <select className="select select-bordered bg-none">
-                            <option disabled selected></option>
-                            <option>T-shirts</option>
-                            <option>Mugs</option>
-                        </select>
+  return state;
+};
 
-                        <span className="pointer-events-none bg-transparent absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-                            </svg>
-                        </span>
-                        {/* <button className="btn">Go</button> */}
-                    </div>
-                </div>
-            </th>
-            {/* Memo */}
-            <th>
-                <input type="text" placeholder="" className="input input-bordered w-full max-w-xs" />
-            </th>
-            {/* Amount */}
-            <th>
-                <div className='flex gap-1 items-center'>
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-4 h-4"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M18 12H6"
-                            />
-                        </svg>
-                    </span>
-                    <input type="text" placeholder="0.00" className="input rounded-full input-sm input-bordered w-full max-w-xs" />
-                </div>
+const reducer = (state, [status, payload]) => {
+  if (status === "success") return { ...state, data: payload, status };
+  // e.g. make sure to reset data, when loading.
+  else if (status === "loading") return { ...state, data: undefined, status };
+  else return state;
+};
 
-            </th>
-            {/* Category */}
+const App = () => {
+  const { data, status } = useData();
+  const count = useRenderCount();
+  const countStr = `Re-rendered ${count.current} times`;
 
-            <th>
-                <div className="input-group relative">
-                    <select className="select select-bordered bg-none">
-                        <option disabled selected></option>
-                        <option>T-shirts</option>
-                        <option>Mugs</option>
-                    </select>
-                    <span className="pointer-events-none bg-transparent absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-                        </svg>
-                    </span>
-                </div>
-            </th>
-            <th>
-                <div className=''>
-                    <div className='flex gap-2'>
-                        {/* no-symbol */}
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
-                        </svg>
-                        {/* check */}
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                        </svg>
-                    </div>
-                </div>
-            </th>
-        </tr>
-    )
+  return status === "loading" ? (
+    <div> Loading (3 sec)... {countStr} </div>
+  ) : (
+    <div>
+      Finished. Data: {JSON.stringify(data)}, {countStr}
+    </div>
+  );
 }
 
-// export function name() {
-//     return (
-//         <tr>
-//             <td>
-//                 <label>
-//                     <input type="checkbox" className="checkbox" />
-//                 </label>
-//             </td>
-//             <th>
-//                 <div className="flex items-center space-x-3">
-//                     <div>
-//                         <div className="font-bold">
-//                             <Link href={`buckets/${bucket.slug}`} className='link link-hover' >
-//                                 {bucket.name}
-//                             </Link>
-//                         </div>
-//                         {/* <div className="text-sm opacity-50">{bucket.description}</div> */}
-//                     </div>
-//                 </div>
-//             </th>
-//             <td>
-//                 <div>
-//                     <div className="font-bold">
-//                         <Link href={`buckets/${bucket.slug}`} className='link link-hover' >
-//                             {bucket.name}
-//                         </Link>
-//                     </div>
-//                     {/* <div className="text-sm opacity-50">{bucket.description}</div> */}
-//                 </div>
-//                 {/* <div className="form-control"> <label className="input-group input-group-sm"> <input type="text" placeholder="0.01" className="input input-bordered input-sm" /> <span>{bucket.budgeted}</span> </label> </div> */}
-//             </td>
-//             <td>
-//                 {monthlySelect()}
-//                 {/* {monthlyToggle()} */}
-//             </td>
-//             <td className=''>
-//                 {/* <RadialProgress value={bucket.available / bucket.budgeted * 100} /> */}
-//             </td>
-//             <td className=''>
-//                 {/* <RadialProgress value={bucket.available / bucket.budgeted * 100} /> */}
-//             </td>
-//             <td>
-//                 <div className=''>
-//                     <div className='flex gap-2'>
-//                         {settings()}
-//                         {statistics()}
-//                     </div>
-//                     <div className='relative'>
-//                         <progress className="progress progress-success bg-base-300" value={bucket.available / bucket.budgeted * 100} max="100"></progress>
-//                         <div className="tooltip tooltip-left absolute left-0 w-full " data-tip={bucket.available / bucket.budgeted * 100}>
-//                             &nbsp;
-//                         </div>
-//                         {/* <RadialProgress value={bucket.available / bucket.budgeted * 100} /> */}
-//                     </div>
-//                 </div>
-//             </td>
-//         </tr>
-//     )
-// }
+//
+// helpers
+//
+
+const useRenderCount = () => {
+  const renderCount = useRef(0);
+  useEffect(() => {
+    renderCount.current += 1;
+  });
+  return renderCount;
+};
+
+const fetchData_fakeApi = () =>
+  new Promise(resolve =>
+    setTimeout(() => resolve({ ok: true, data: { results: [1, 2, 3] } }), 3000)
+  );
+
+ReactDOM.render(<App />, document.getElementById("root")); */
