0f1d0883edbc9e13358997247f1d741f4daab7dc
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-11-10 11:14:36 +0530

    style(components): add secondary stats for accounts table

--- components/AccountsTable.tsx
+++ components/AccountsTable.tsx
@@ -1,42 +1,71 @@
 import React, { useState } from 'react'
-import Link from 'next/link'
 import { data } from '../utils/data'
-import AccountsHeader from './account/AccountsHeader'
 import { transactions } from '../utils/transactions'
-import { PageHeading } from './PageHeading'
-import TransactionsTable from './TransactionsTable'
-import ButtonGroupAccounts from './ui/ButtonGroupAccounts'
 
 export default function AccountsTable() {
-
-    const [disabled, setDisabled] = useState(false)
-    const handleClickEditTransaction = () => { setDisabled(!disabled) }
-
-    const handleClickClearTransaction = () => { }
+    // const [disabled, setDisabled] = useState(false)
+    // const handleClickEditTransaction = () => { setDisabled(!disabled) }
+    // const handleClickClearTransaction = () => { }
+    const today = new Date().toDateString()
     return (
         <>
             {/* TODO: Make table head sticky */}
             <div className='px-6 border-b border-neutral'>
                 {data.accounts.map((account) => {
-                    if (account.name === 'Bank of Dodge') {
+                    if (account.name === 'Bank of Westeros') {
                         return (
                             <>
-                                <div className='text-xl uppercase'>{account.type}</div>
-                                <div className=''>{account.balance}</div>
-                                <div className='form-control'>
-                                    <div className=''> In </div>
-                                    <div className=''> Out </div>
-                                    <div className=''> Net </div>
-                                    <div className="input-group relative">
-                                        <select className="select select-sm w-fit select-bordered bg-none">
-                                            <option disabled selected></option>
-                                            {transactions.map((transaction) => { return (<option>{transaction.category}</option>) })}
-                                        </select>
-                                        <span className="pointer-events-none bg-transparent absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                                            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" > <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" /> </svg>
-                                        </span>
+                                <div className="mb-4 gap-4">
+                                    <div className='text-xl uppercase'>{account.type}</div>
+                                    <div className="stats w-full mb-4 shadow-inner">
+                                        <div className="stat">
+                                            <div className="hidden stat-figure text-secondary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                            </div>
+                                            <div className="stat-title">Balance</div>
+                                            <div className="stat-value text-base">{account.balance}</div>
+                                            <div className="stat-desc">{today}</div>
+
+                                        </div>
+                                        <div className="stat">
+                                            <div className="hidden stat-figure text-secondary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                            </div>
+                                            <div className="stat-title">In</div>
+                                            <div className="stat-value text-base">31.00</div>
+                                            <div className="stat-desc">Jan 1st - Feb 1st</div>
+                                        </div>
+                                        <div className="stat">
+                                            <div className="hidden stat-figure text-secondary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+                                            </div>
+                                            <div className="stat-title">Out</div>
+                                            <div className="stat-value text-base">4,200.00</div>
+                                            <div className="stat-desc">↗︎ 400 (22%)</div>
+                                        </div>
+                                        <div className="stat">
+                                            <div className="hidden stat-figure text-secondary">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                                            </div>
+                                            <div className="stat-title">Net</div>
+                                            <div className="stat-value text-base">1,200.00</div>
+                                            <div className="stat-desc">↘︎ 90 (14%)</div>
+                                        </div>
+                                        <div className='stat'>
+                                            <div className="form-control gap-4">
+                                                <div className="input-group input-group-sm grid grid-cols-3 items-center">
+                                                    <select className="select select-sm select-bordered col-span-2 w-full">
+                                                        <option disabled selected>Pick Type</option>
+                                                        <option>Normal</option>
+                                                        <option>Off-budget</option>
+                                                        <option>Debt</option>
+                                                    </select>
+                                                    <button className="btn btn-sm bg-base-300">Go</button>
+                                                </div>
+                                                <button className='btn btn-sm bg-base-300'>Actions</button>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <button className='btn w-fit'>Actions</button>
                                 </div>
                             </>
                         )
